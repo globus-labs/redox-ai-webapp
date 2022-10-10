2cc2ca8e90bd21548f8afad441d833555aa8bbdf
Logan Ward <ward.logan.t@gmail.com>
2022-10-10 16:16:48 -0400

    Return prediction results via WS

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from typing import Tuple, Optional
 from pathlib import Path
 import logging
 
+from pydantic import BaseModel, Field
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem.Draw import MolDraw2DSVG
 from fastapi import FastAPI
+from fastapi.websockets import WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, HTMLResponse
 
@@ -13,6 +16,16 @@ html_dir = Path(__file__).parent / 'html'
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 logger = logging.getLogger('app')
+
+
+class PredictionResult(BaseModel):
+    """Result of inference on a DLHub model"""
+
+    smiles: str = Field(..., help='SMILES string of molecule passed to the client')
+    key: str = Field(..., help='InChI key of the molecule')
+    model_name: str = Field(..., help='Name of the model which was invoked')
+    value: float = Field(..., help='Output value')
+    confidence_interval: Optional[Tuple[float, float]] = Field(None, help='Confidence intervals, if known')
 
 
 def _print_molecule(smiles) -> str:
@@ -37,9 +50,41 @@ def _print_molecule(smiles) -> str:
 
 @app.get("/")
 def read_root():
+    """Serve the home page"""
     return FileResponse(html_dir / 'home.html')
 
 
 @app.get('/api/render')
-def render_molecule(smiles: str):
+def render_molecule(smiles: str) -> HTMLResponse:
+    """Render a molecule"""
     return HTMLResponse(_print_molecule(smiles))
+
+
+@app.websocket('/ws')
+async def result_messenger(socket: WebSocket):
+    """Open a socket connection that will manage sending results to-and-from the system
+
+    Args:
+        socket: The websocket created for this particular session
+    """
+
+    # Accept the connection
+    await socket.accept()
+    logger.info(f'Connected to client at {socket.client.host}')
+
+    try:
+        while True:
+            # Wait for messages from the webapp to arrive
+            msg = await socket.receive_json()
+            smiles = msg['smiles']
+
+            # Send a result back
+            result = PredictionResult(
+                smiles=smiles,
+                key='aaa' * 4,
+                model_name='solv_ml',
+                value=1.
+            )
+            await socket.send_text(result.json())
+    except WebSocketDisconnect:
+        logger.info(f'Disconnected from client at {socket.client.host}')
--- static/redox.js
+++ static/redox.js
@@ -1,11 +1,49 @@
 'use strict';
 
+// We use a websocket to send results from the web server as they complete
+var socket = new WebSocket("ws://127.0.0.1:8000/ws")
+
+// Get the current molecule
+function activeMolecule() {
+    return document.getElementById("smiles").value;
+}
+
+// Function that begins a computation
 function compute() {
     // Get the name of the molecule
-    var mol = document.getElementById("smiles").value;
+    var mol = activeMolecule();
 
+    // Render it as an SVG
     axios.get("/api/render", {params: {smiles: mol}})
         .then(function (res) {
             document.getElementById("mol-img").innerHTML = res.data
         })
+
+    // Request its properties
+    socket.send(JSON.stringify({"smiles": mol}))
 }
+
+
+// Functions
+socket.onmessage = function(event) {
+    /** Set the appropriate molecule property **/
+    var data = JSON.parse(event.data);
+    var smiles = data.smiles;
+
+    // Make sure the molecule hasn't changed
+    var set_smiles = activeMolecule();
+    if (set_smiles != smiles) {
+        console.log('Molecule has updated')
+        return
+    }
+
+    // Update the appropriate row
+    var model_id = data.model_name;
+    var value = data.value;
+    var result_row = document.getElementById(model_id);
+    if (! result_row) {
+        return console.log(`[error] result row not found ${model_id}`)
+    }
+    result_row.cells[1].innerHTML = value
+}
+
