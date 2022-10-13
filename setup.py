bedb0cfc20cf0e5dc2a6c7ce3d7e31269f963948
Logan Ward <ward.logan.t@gmail.com>
2022-10-13 15:13:07 -0400

    Add a procfile and gunicorn

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 
+# this grabs the requirements from requirements.txt
+requirements = [i.strip() for i in open("requirements.txt").readlines()]
+
 setup(
     name='redoxweb',
     version='0.0.1',
     packages=find_packages(),
     description='Web service for designing molecules online',
-    install_requires=[
-        'rdkit-pypi',
-        'fastapi',
-        'dlhub-sdk',
-        'foundry_ml'
-    ]
+    install_requires=requirements
 )
