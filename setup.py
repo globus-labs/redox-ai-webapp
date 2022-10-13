32db1e7a4b4210725ef515035805056ff67fa981
Logan Ward <ward.logan.t@gmail.com>
2022-10-13 15:03:18 -0400

    Add requirements to setup, dark format for navbar
    Yes, these should probably be separate commits

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setup(
     name='redoxweb',
     version='0.0.1',
     packages=find_packages(),
-    description='Web service for designing molecules online'
+    description='Web service for designing molecules online',
+    install_requires=[
+        'rdkit-pypi',
+        'fastapi',
+        'dlhub-sdk',
+        'foundry_ml'
+    ]
 )
